dbb5c49a2ec454db72d48c162591bce1002e593e
Karthik-Kollur <karthik.kollur.jsr@gmail.com>
2023-10-04 14:33:17 +0530

    product screen

---
+++ src/Screens/ProductsScreen.js
@@ -0,0 +1,22 @@
+import { FlatList, View, StyleSheet, Image } from "react-native";
+import products from "../data/products";
+
+const ProductScreen = () => {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <View style={{ width: "50%", padding: 1 }}>
+          <Image source={{ uri: item.image }} style={styles.image} />
+        </View>
+      )}
+      numColumns={2}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  image: { width: "100%", aspectRatio: 1 },
+});
+
+export default ProductScreen;
